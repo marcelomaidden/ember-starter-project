484790af6e74919aeed776489a0a677c11eff16e
Marcelo Fernandes de Araujo <marcelomaidden@gmail.com>
2021-03-03 21:10:25 -0300

    Ask Question: Checks if user is allowed to update question

--- app/routes/questions.js
+++ app/routes/questions.js
@@ -1,10 +1,26 @@
 import Route from '@ember/routing/route';
+import { inject } from "@ember/service";
+import AuthenticatedRouteMixin from "ember-simple-auth/mixins/authenticated-route-mixin";
 
-export default Route.extend({
+export default Route.extend(AuthenticatedRouteMixin, {
+  currentSession: inject(),
   model(params) {
-    if (params.id)
-      return this.store.findRecord('question', params.id);
-    else
-      return this.store.createRecord('question');
+    return this.store.findRecord('question', params.id);
   },
+  actions: {
+    async update() {
+      const question = this.controller.model;
+      if (Number(this.currentSession.user.id) !== Number(question.user.id))
+        this.controller.set("errorMessage", 'User is not allowed to change this question');
+      else {
+        await question.save()
+        .then(() => this.transitionTo('index'))
+        .catch(
+          () => {
+            this.controller.set("errorMessage", 'Please, fix all errors and try again');
+          }
+        )
+      }
+    }
+  }
 });
